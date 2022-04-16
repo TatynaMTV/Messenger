a9eabd17fb5fe7fd9678cc2cab8d1ec696bb93d4
TatynaMTV <89783451+TatynaMTV@users.noreply.github.com>
2022-04-16 14:50:30 +0300

    Location Messages

---
+++ Messenger/Controllers/LocationPickerViewController.swift
@@ -0,0 +1,81 @@
+//
+//  LocationPickerViewController.swift
+//  Messenger
+//
+//  Created by Татьяна Мальчик on 13.04.2022.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class LocationPickerViewController: UIViewController {
+    
+    public var completion: ((CLLocationCoordinate2D) -> Void)?
+    private var coordinates: CLLocationCoordinate2D?
+    private var isPickable = true
+    
+    private let map: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    
+    init(coordinates: CLLocationCoordinate2D?) {
+        self.coordinates = coordinates
+        self.isPickable = false
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(map)
+        map.isUserInteractionEnabled = true
+        
+        if isPickable {
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Send", style: .done, target: self, action: #selector(sendButtonTapped))
+            
+            let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapMap(_:)))
+            gesture.numberOfTapsRequired = 1
+            gesture.numberOfTouchesRequired = 1
+            map.addGestureRecognizer(gesture)
+        } else {
+            // just showing location
+            guard let coordinates = self.coordinates else { return }
+            // drop a pin on thet location
+            let pin = MKPointAnnotation()
+            pin.coordinate = coordinates
+            map.addAnnotation(pin)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        map.frame = view.bounds
+    }
+    
+    @objc func sendButtonTapped() {
+        guard let coordinates = coordinates else { return }
+        navigationController?.popViewController(animated: true)
+        completion?(coordinates)
+    }
+    
+    @objc func didTapMap(_ gesture: UITapGestureRecognizer){
+        let locationInView = gesture.location(in: map)
+        let coordinates = map.convert(locationInView, toCoordinateFrom: map)
+        self.coordinates = coordinates
+        
+        for annotation in map.annotations {
+            map.removeAnnotation(annotation)
+        }
+        
+        // drop a pin on thet location
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinates
+        map.addAnnotation(pin)
+    }
+}
--- Messenger/Resources/DatabaseManager.swift
+++ Messenger/Resources/DatabaseManager.swift
@@ -8,6 +8,7 @@
 import Foundation
 import FirebaseDatabase
 import MessageKit
+import CoreLocation
 
 final class DatabaseManager {
     
@@ -377,6 +378,11 @@ extension DatabaseManager {
                           let placeholder = UIImage(named: "plaseholder") else { return nil}
                     let media = Media(url: videoUrl, image: nil, placeholderImage: placeholder, size: CGSize(width: 300, height: 300))
                     kind = .video(media)
+                } else if type == "location" {
+                    let locationComponents = content.components(separatedBy: ",")
+                    guard let latitude = Double(locationComponents[0]), let longitude = Double(locationComponents[1]) else { return nil }
+                    let location = Location(location: CLLocation(latitude: latitude, longitude: longitude), size: CGSize(width: 300, height: 300))
+                    kind = .location(location)
                 } else {
                     kind = .text(content)
                 }
@@ -430,7 +436,9 @@ extension DatabaseManager {
                     message = targetUrlString
                 }
                 break
-            case .location(_):
+            case .location(let locationData):
+                let location = locationData.location
+                message = "\(location.coordinate.latitude), \(location.coordinate.longitude)"
                 break
             case .emoji(_):
                 break
